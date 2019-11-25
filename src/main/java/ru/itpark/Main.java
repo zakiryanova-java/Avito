86338cedf17a0dde928fd397e0d98825cb6e9bc2
zakiryanova <zakiryanova@localhost>
2019-11-25 22:14:11 +0300

    fixes #2

--- src/main/java/ru/itpark/Main.java
+++ src/main/java/ru/itpark/Main.java
@@ -1,6 +1,7 @@
 package ru.itpark;
 
 import ru.itpark.model.House;
+import ru.itpark.repository.HouseRepositoryJdbcImpl;
 import ru.itpark.service.HouseService;
 
 import java.sql.SQLException;
@@ -8,12 +9,10 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
-        HouseService service = new HouseService();
+        final HouseService service = new HouseService(new HouseRepositoryJdbcImpl("jdbc:sqlite:db.sqlite"));
         List<House> result = service.searchByDistrict("Приволжский", "Цена по возрастанию");
         System.out.println(result);
         List<House> resultOther = service.searchByDistrict("Приволжский", "Цена по убыванию");
         System.out.println(resultOther);
-
-
     }
 }
--- src/main/java/ru/itpark/service/HouseService.java
+++ src/main/java/ru/itpark/service/HouseService.java
@@ -1,39 +1,27 @@
 package ru.itpark.service;
 
 import ru.itpark.model.House;
-import ru.itpark.util.JdbcTemplate;
+import ru.itpark.repository.HouseRepository;
 
 import java.sql.SQLException;
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class HouseService {
-    private List<House> houses = JdbcTemplate.executeQuery("jdbc:sqlite:db.sqlite", "SELECT id, price, district, underground FROM houses",
-            resultSet -> new House(
-                    resultSet.getInt("id"),
-                    resultSet.getInt("price"),
-                    resultSet.getString("district"),
-                    resultSet.getString("underground")
-            ));
+    private final HouseRepository repository;
 
-    public HouseService() throws SQLException {
+    public HouseService(HouseRepository repository) {
+        this.repository = repository;
     }
 
-    public List<House> searchByDistrict(String district, String sortBy) {
-        List<House> list = new ArrayList<>();
-        for (House house : houses) {
-            if (house.getDistrict().equals(district)) {
-                list.add(house);
-            }
-        }
+    public List<House> searchByDistrict(String district, String sortBy) throws SQLException {
         if (sortBy.equals("Цена по возрастанию")) {
-            Comparator<House> houseAscComparator = Comparator.comparing(h -> h.getPrice());
-            list.sort(houseAscComparator);
+            return repository.searchByDistrict(district).stream().sorted((o1, o2) -> o1.getPrice() - o2.getPrice()).collect(Collectors.toList());
         }
         if (sortBy.equals("Цена по убыванию")) {
-            Comparator<House> houseDescComparator = Comparator.comparing(House::getPrice).reversed();
-            list.sort(houseDescComparator);
+            return repository.searchByDistrict(district).stream().sorted((o1, o2) -> -(o1.getPrice() - o2.getPrice())).collect(Collectors.toList());
         }
-        return list;
+        return null;
     }
 }
 
